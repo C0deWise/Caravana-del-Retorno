503429032b36e092d99a92653ca177dd5ae3512e
ashlee <ashleecampaz@unicauca.edu.co>
2026-03-26 21:51:54 -0500

    doc: se agrego la documentacion de registro usuario

--- app/usuarios/api/v1/usuario_router.py
+++ app/usuarios/api/v1/usuario_router.py
@@ -12,7 +12,7 @@ from app.core.database import get_db
 from app.usuarios.repository.usuario_repositorio import UsuarioRepositorio
 from app.usuarios.services.usuario_servicio import UsuarioServicio
 from app.usuarios.schemas.usuario_esquemas import UsuarioCrear, UsuarioSalida, UsuarioNombre, UsuarioDetallado
-
+from app.usuarios.docs.registro_doc import registrar_body, registrar_docs
 router = APIRouter(prefix="/usuario", tags=["Usuario"])
 
 
@@ -33,11 +33,11 @@ def get_usuario_servicio(db: AsyncSession = Depends(get_db)) -> UsuarioServicio:
 
 @router.post(
     "/registrar",
-    status_code=status.HTTP_201_CREATED,
+    status_code=status.HTTP_201_CREATED,**registrar_docs
 )
 async def registrar_usuario(
-    schema: UsuarioCrear,
-    servicio: UsuarioServicio = Depends(get_usuario_servicio),
+    schema: registrar_body,
+    servicio: UsuarioServicio = Depends(get_usuario_servicio), 
 ):
     try:
         usuario = await servicio.registrar(schema)
