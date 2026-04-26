bd4ffaff18a78747aa881e6d425afede15964af5
AIQuira <166058744+AIQuira@users.noreply.github.com>
2026-04-25 23:28:42 -0500

    feat(3GC-6): Cambiar líder de una colonia.

--- app/colonias/repositories/colonia_repository.py
+++ app/colonias/repositories/colonia_repository.py
@@ -5,6 +5,7 @@ relacionadas con colonias colombianas, utilizando sesiones SQLAlchemy
 como capa de persistencia.
 """
 
+from app.usuarios.models import usuario
 from app.usuarios.models.usuario import Usuario
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -147,6 +148,17 @@ class ColoniaRepository:
         """
         colonia.estado = ColoniaEstado.INACTIVA
         colonia.lider = None
+        await self.db.commit()
+        await self.db.refresh(colonia)
+        return colonia
+
+    async def cambiar_lider_colonia(self, colonia_codigo: int, nuevo_lider_id: int) -> Colonia:
+        colonia = await self.obtener_colonia_por_id(colonia_codigo)
+        usuario_antiguo = await self.db.get(Usuario, colonia.lider)
+        usuario_antiguo.ro_codigo = 1
+        usuario_nuevo = await self.db.get(Usuario, nuevo_lider_id)
+        usuario_nuevo.ro_codigo = 2
+        colonia.lider = nuevo_lider_id
         await self.db.commit()
         await self.db.refresh(colonia)
         return colonia
